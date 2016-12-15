04c8608cb3c5249ab54078344c338b56823b5c2a
Tanner Perrien <weslee3@gmail.com>
2016-12-14 22:41:54 -0800

    Added enrolled to form. Added unsubscribe.

--- page.js
+++ page.js
@@ -3,20 +3,6 @@ if (window.location.host == host && window.location.protocol != 'https:') {
   window.location.protocol = 'https:';
 }
 
-$(function() {
-  $('#signup').click(function() {
-    $('#signupContainer').collapse('hide');
-  });
-  
-  $('#form').on('submit', function(e) {
-    e.preventDefault();
-    $.post('https://stark-lake-20962.herokuapp.com/save', $(this).serialize(), function() {
-      $('#formContainer').collapse('hide');
-      $('#formComplete').collapse('show');
-    });
-  });
-});
-
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -24,3 +10,43 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 
 ga('create', 'UA-88872400-1', 'auto');
 ga('send', 'pageview');
+
+$(function() {
+  $('#signup').click(function() {
+    $('#signupContainer').collapse('hide');
+  });
+  
+  $('#formSubscribe').on('submit', function(e) {
+    e.preventDefault();
+    var url = 'https://stark-lake-20962.herokuapp.com/subscribe';
+    url = 'http://localhost:5000/subscribe';
+    var data = $(this).serialize();
+    var res = Promise.resolve($.post(url, data)).then(function(res) {
+      if (res.error) return showError(res.error);
+      $('#subscribeContainer').collapse('hide');
+      $('#subscribeComplete').collapse('show');
+    }).catch(function(err) {
+      showError(err);
+    });
+  });
+  
+  function showError(err) {
+    $('#error-container').show();
+    $('#error-msg').text(err);
+  }
+  
+  $('#formUnsubscribe').on('submit', function(e) {
+    e.preventDefault();
+    var url = 'https://stark-lake-20962.herokuapp.com/unsubscribe';
+    url = 'http://localhost:5000/unsubscribe';
+    var data = $(this).serialize();
+    var res = Promise.resolve($.post(url, data)).then(function(res) {
+      $('#unsubscribeContainer').collapse('hide');
+      $('#unsubscribeComplete').collapse('show');
+    }).catch(function(err) {
+      $('#unsubscribeContainer').collapse('hide');
+      $('#unsubscribeComplete').collapse('show');
+    });
+  });
+});
+
